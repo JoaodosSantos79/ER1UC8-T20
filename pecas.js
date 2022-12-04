d7f990b0d8c04337bc07a60374df1953cb6c27cb
Joao <joao.carlos.sts@gmail.com>
2022-12-04 15:48:36 -0300

    renomeação do arquivo

--- pecas.js
+++ pecas.js
@@ -1,38 +1,32 @@
+
 let pesoPeca = 200
+
+if (pesoPeca > 100) {
+    console.log("Peso adequado, podemos cadastrar")
+} else {
+  console.log("Cadastro não permitido, peso fora da especificação")  
+}
+
 let numeroPecas = 10
+
+if (numeroPecas >= 10) {
+    console.log("Capacidade máxima atingida")
+} else {
+  console.log("Ainda há espaço para cadastro")
+}
+
 let listaPecas = ["Disco de freio","Motor","Pastilha de freio","Pneu","Volante"]
+
+if (listaPecas.length >= 10) {
+  console.log("Capacidade máxima atingida")    
+} else {
+  console.log("Ainda há espaço para cadastro")
+}
+
 let nomePeca = "Disco de freio"
 
- //Verificação peso da peça
-if (pesoPeca > 100) {
-    // Podemos cadastrar
-  console.log ("Peso adequado, podemos cadastrar")
-
-} else {
-    // Não podemos cadastrar
-  console.log ("Peso menor que 100g, não podemos cadastrar")  
-}
- //Verificação tammanho da lista de peças
-if (numeroPecas >= 10) {
-    //Capacidade máxima atingida
-    console.log ("Capacidade máxima atingida")
-} else {
-    //  Ainda há espaço para cadastro
-  console.log ("Ainda há espaço para cadastro")
-}
- //Verificação da lista de peças
-if (listaPecas.length >= 10) {
-    //Capacidade máxima atingida
-  console.log ("Capacidade máxima atingida")    
-} else {
-    // Ainda há espaço para cadastro
-  console.log ("Ainda há espaço para cadastro")
-}
- //Verificação do nome da peça
 if (nomePeca.length < 3) {
-    // Nome inválido
-   console.log ("Nome inválido, o nome precisa ter 3 caracteres ou mais")   
+   console.log("Nome inválido, o nome precisa ter 3 caracteres ou mais")   
 } else {
-    // Nome da peça adequado
-    console.log ("Nome adequado")
+    console.log("Nome adequado")
 }
